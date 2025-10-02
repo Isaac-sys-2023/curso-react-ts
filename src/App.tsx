1917b234a1350352e3b87fa47ecb7a3e2830a802
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-10-02 19:48:09 -0400

    feat: Creacion de componente Presentacion de ejemplo

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,14 @@ import './App.css'
 import Nombre from './componentes/Nombre';
 import viteLogo from "../public/vite.svg";
 
+import Presentacion from "./src_isaac_gh/Presentacion";
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Isaac Gutierrez Huarachi</h1>
-      <Nombre precio={20} titulo={"Hola"} descripcion={"Como estas"}/>
-
-      <Nombre precio={30} titulo={"Vaso"} descripcion={"Vaso de vidrio"}/>
-      <Nombre precio={20} titulo={"Hola"} descripcion={"Como estas"}/>
-      <Nombre precio={20} titulo={"Hola"} descripcion={"Como estas"}/>
+      <Presentacion/>
     </>
   )
 }
---
+++ src/src_isaac_gh/Presentacion.tsx
@@ -0,0 +1,13 @@
+//Su nombre, su edad y una descripcion suya con props
+
+const Presentacion = () =>{
+    return(
+        <>
+        <h1>Isaac</h1>
+        <h2>Edad</h2>
+        <h3>Descripcion suya</h3>
+        </>        
+    )
+}
+
+export default Presentacion;
