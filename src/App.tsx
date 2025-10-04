f89334154ebba3cb3c6d819bd3bf6c4f2904dfab
Yahir <leonardocayola@gmail.com>
2025-10-03 23:39:15 -0400

    feat: Creacion de componente tarea Yahir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Presentacion from './src_laura_ml/Presentacion'
 import PresentacionOAV from './src_orlando_av/PresentacionOAV'
-
+import PresentacionYLCC from './src_yahir_cc/PresentacionYLCC'
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <h1>Isaac Gutierrez Huarachi</h1>
       <Presentacion nombre='Laura' descripcion='me gusta la robotica y dormir' edad={25}/>
       <PresentacionOAV nombre='Orlando' descripcion='Estudiante de Sistemas' edad={23}/>
+      <PresentacionYLCC nombre='Yahir' descripcion='Estudiante de Sistemas me gusta entrenar' edad={22}/>
     </>
     
   )
