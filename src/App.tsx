d18b6d19bf3e481495aa737d0a3d39049d562254
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-10-04 20:01:37 -0400

    feat: Importando el codigo de clase para hacerlo visible en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
+
 import './App.css'
 import Presentacion from './src_laura_ml/Presentacion'
 import PresentacionOAV from './src_orlando_av/PresentacionOAV'
 import PresentacionYLCC from './src_yahir_cc/PresentacionYLCC'
 
+import AppTutor from './src_isaac_gh/AppTutor'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Isaac Gutierrez Huarachi</h1>
-      <Presentacion nombre='Laura' descripcion='me gusta la robotica y dormir' edad={25}/>
-      <PresentacionOAV nombre='Orlando' descripcion='Estudiante de Sistemas' edad={23}/>
-      <PresentacionYLCC nombre='Yahir' descripcion='Estudiante de Sistemas me gusta entrenar' edad={22}/>
+      <AppTutor/>
+      
+      <Presentacion nombre='Laura' descripcion='me gusta la robotica y dormir' edad={25} />
+      <PresentacionOAV nombre='Orlando' descripcion='Estudiante de Sistemas' edad={23} />
+      <PresentacionYLCC nombre='Yahir' descripcion='Estudiante de Sistemas me gusta entrenar' edad={22} />
     </>
-    
+
   )
 }
 
