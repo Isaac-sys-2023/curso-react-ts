d502e18c7498fd4310f2dcb88a1478dcd28c1473
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-10-07 20:35:34 -0400

    feat: Añadiendo todas la imagenes necesarias para los cursos

---
+++ src/assets/afiches/afiches.ts
@@ -0,0 +1,23 @@
+import imgAutocadAfiche from "./FB_IMG_1759843000753.jpg";
+import imgHTMLAfiche from "./FB_IMG_1759843003306.jpg";
+import imgAndroidAfiche from "./FB_IMG_1759843006219.jpg";
+import imgLinuxAfiche from "./FB_IMG_1759843007998.jpg";
+import imgPythonAfiche from "./FB_IMG_1759843010130.jpg";
+import imgManchineAfiche from "./FB_IMG_1759843018121.jpg";
+import imgLangChainAfiche from "./FB_IMG_1759843020772.jpg";
+import imgReactAfiche from "./FB_IMG_1759843023192.jpg";
+import imgBackendAfiche from "./FB_IMG_1759843025507.jpg";
+import imgGodotAfiche from "./FB_IMG_1759843027784.jpg";
+
+export const afichesAssets = {
+    imgAutocadAfiche,
+    imgAndroidAfiche,
+    imgBackendAfiche,
+    imgGodotAfiche,
+    imgHTMLAfiche,
+    imgLangChainAfiche,
+    imgLinuxAfiche,
+    imgManchineAfiche,
+    imgPythonAfiche,
+    imgReactAfiche
+}
---
+++ src/assets/cursos/cursos.ts
@@ -0,0 +1,23 @@
+import imgAutocadCursos from "./autocad.png";
+import imgHTMLCursos from "./html.png";
+import imgAndroidCursos from "./android.png";
+import imgLinuxCursos from "./linux.jpeg";
+import imgPythonCursos from "./python.jpeg";
+import imgManchineCursos from "./machine.jpeg";
+import imgLangChainCursos from "./langchain.png";
+import imgReactCursos from "./react.png";
+import imgBackendCursos from "./node.png";
+import imgGodotCursos from "./godot.png";
+
+export const cursosAssets = {
+    imgAutocadCursos,
+    imgAndroidCursos,
+    imgBackendCursos,
+    imgGodotCursos,
+    imgHTMLCursos,
+    imgLangChainCursos,
+    imgLinuxCursos,
+    imgManchineCursos,
+    imgPythonCursos,
+    imgReactCursos
+}
