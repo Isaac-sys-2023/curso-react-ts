ef3afa074951f9bc27b3203c6710724763bf242f
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-10-15 20:43:17 -0400

    feat: uso de fetch consumo de Apis

--- src/src_orlando_av/views/TareaView4.tsx
+++ src/src_orlando_av/views/TareaView4.tsx
@@ -8,7 +8,7 @@ interface Dog {
 const TareaView4 = () => {
   const [count, setCount] = useState<number>(0);
 
-  const [dogs, setDogs] = useState<Dog[]>([]);
+  const [dogs, setDogs] = useState<Dog>({ message: "Que alguien detenga el semestre", status: "error" });
 
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
@@ -19,7 +19,7 @@ const TareaView4 = () => {
 
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((res) => {
-        if (!res.ok) throw new Error("EError al cargar datos");
+        if (!res.ok) throw new Error("Error al cargar datos");
         console.log("Llamamos a la Api");
         return res.json();
       })
@@ -36,20 +36,16 @@ const TareaView4 = () => {
   }, [count]);
 
   if (loading === true) return <p>Cargando usuarios...</p>;
-  if (error != "") return <p style={{ color: "red" }}>{error}</p>;
+  if (error != "") return <p style={{ color: "cyan" }}>{error}</p>;
 
   return (
     <div>
       <h2>Clicks: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Aumentar</button>
-
       <div>
-        <h2>Lista de users</h2>
-        <ul>
-          {dogs.map((dog, index) => (
-            <li key={index}>{dog.message}</li>
-          ))}
-        </ul>
+        <h2>Perro:</h2>
+        <img src={dogs.message} alt="Perro aleatorio" style={{ maxWidth: "500px" }} />
+        <h3>status: {dogs.status}</h3>
       </div>
     </div>
   );
