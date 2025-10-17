801f08b9fc510c4552ef04aba8b2879fed2e82a4
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-10-17 00:34:31 -0400

    feat: Formularios de registro Fb y Tk

--- src/src_orlando_av/AppOrlando.tsx
+++ src/src_orlando_av/AppOrlando.tsx
@@ -5,6 +5,8 @@ import Tarea0View from './views/Tarea0View';
 import DetalleTarea2 from './componentes/DetalleTarea2';
 import HomeOAVView from "./views/HomeOAVView";
 import TareaView4 from "./views/TareaView4";
+import Tarea5ViewRegistro from "./views/Tarea5ViewRegistro";
+import Tarea5ViewLogin from "./views/Tarea5ViewLogin";
 
 const TareasOAV = () => {
   return (
@@ -15,6 +17,8 @@ const TareasOAV = () => {
         <Route path="tarea1" element={<Tarea1View />} />
         <Route path="tarea2" element={<Tarea2View />} />
         <Route path="tarea4View" element={<TareaView4 />} />
+        <Route path="tarea5Login" element={<Tarea5ViewLogin />} />
+        <Route path="tarea5Registro" element={<Tarea5ViewRegistro />} />
         <Route
           path="tarea2/:id/:titulo/:descripcion"
           element={<DetalleTarea2 />}
