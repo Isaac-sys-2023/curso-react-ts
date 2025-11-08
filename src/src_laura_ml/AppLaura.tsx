1ff0cd1309aa6b94205efafb03371c41aead0b57
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-11-08 00:49:43 -0400

    fix: Arreglando el error de rutas en AppLaura.tsx

--- src/src_laura_ml/AppLaura.tsx
+++ src/src_laura_ml/AppLaura.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import TareaView5 from "/curso-react-ts/src/src_laura_ml/view/TareaView5";
+import TareaView5 from "./view/TareaView5";
 import TareaView5p2 from "./componentes/TareaView5p2";
 
 function AppLaura() {
